ba844130a86b968b0a7f357a451ec5247298ffe4
ninasrzic <srzicn@gmail.com>
2026-06-04 16:17:17 +0200

    Add project files.

---
+++ Azil.App/Program.cs
@@ -0,0 +1,32 @@
+using Azil.App.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ Azil.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Azil.Shared
+{
+    public class Class1
+    {
+
+    }
+}
